fd9a5b79bc0fdf99745a4efc7d4adaaa074c92b9
JTShocks <jacobdreyer2002@gmail.com>
2023-03-20 15:23:10 -0400

    Challenge2 Initial Commit

--- PlatformerTutorial/Assets/scr_playerController.cs
+++ PlatformerTutorial/Assets/scr_playerController.cs
@@ -9,14 +9,26 @@ public class scr_playerController : MonoBehaviour
 private Rigidbody2D rb;
 public float speed;
 public TextMeshProUGUI score;
+public TextMeshProUGUI lives;
 private int scoreValue = 0;
+private int livesCount = 0;
+public AudioClip itemGrab;
+public AudioClip musicClip;
+public AudioSource musicSource;
 
+public Animator anim;
+private bool facingRight = true;
+private bool isJumping;
+private bool isRunning;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         score.text = scoreValue.ToString();
+        livesCount = 3;
+        lives.text = livesCount.ToString();
     }
 
     // Update is called once per frame
@@ -25,14 +37,36 @@ private int scoreValue = 0;
         float hozMovement = Input.GetAxis("Horizontal");
         float vertMovement = Input.GetAxis("Vertical");
         rb.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+
+        if(hozMovement != 0)
+        {
+            anim.SetInteger("State", 2);
+        }
+        else
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (facingRight == false && hozMovement > 0)
+        {
+            Flip();
+
+        }
+        else if (facingRight == true && hozMovement < 0)
+        {
+            Flip();
+
+        }
     }
-        private void OnCollisionEnter2D(Collision2D collision)
+        private void OnTriggerEnter2D(Collider2D trigger)
     {
-       if (collision.collider.tag == "Coin")
+       if (trigger.tag == "Coin")
         {
             scoreValue += 1;
             score.text = scoreValue.ToString();
-            Destroy(collision.collider.gameObject);
+            musicSource.clip = itemGrab;
+            musicSource.Play();
+            Destroy(trigger.gameObject);
         }
 
     }
@@ -42,8 +76,23 @@ private int scoreValue = 0;
         {
             if (Input.GetKey(KeyCode.W))
             {
-                rb.AddForce(new Vector2(0, 3), ForceMode2D.Impulse); //the 3 in this line of code is the player's "jumpforce," and you change that number to get different jump behaviors.  You can also create a public variable for it and then edit it in the inspector.
+                rb.AddForce(new Vector2(0, 3), ForceMode2D.Impulse); //the 3 in this line of code is the player's "jumpforce," and you change that number to get different jump behaviors
+                while (rb.velocity.y > 0)
+                {
+                    anim.SetInteger("State", 3);
+                }
             }
+            else
+            {
+            anim.SetInteger("State", 0);
+            }   
         }
+    }
+    private void Flip()
+    {
+        facingRight =! facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x *-1;
+        transform.localScale = Scaler;
     }
 }
